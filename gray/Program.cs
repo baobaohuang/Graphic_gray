cd1d9b543762c23a8fc232e21f8de15dd810e8b2
baobaohuang <huangnf95@gmail.com>
2015-02-02 14:48:39 +0800

    graphic_gray
    grayscale

---
+++ gray/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+//C# Tutorial - Convert a Color Image to Grayscale
+//http://tech.pro/tutorial/660/csharp-tutorial-convert-a-color-image-to-grayscale
+//
+namespace gray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bitmap imageSource = new Bitmap(@"C:\success.jpg", true);
+            Bitmap imageOutput ;
+            imageOutput= MakeGrayscale2(imageSource);
+            imageOutput.Save(@"C:\test.jpg", ImageFormat.Jpeg);
+        }
+
+        public static Bitmap MakeGrayscale2(Bitmap original)
+        {
+            unsafe
+            {
+                //create an empty bitmap the same size as original
+                Bitmap newBitmap = new Bitmap(original.Width, original.Height);
+
+                //lock the original bitmap in memory
+                BitmapData originalData = original.LockBits(
+                   new Rectangle(0, 0, original.Width, original.Height),
+                   ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+
+                //lock the new bitmap in memory
+                BitmapData newData = newBitmap.LockBits(
+                   new Rectangle(0, 0, original.Width, original.Height),
+                   ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
+
+                //set the number of bytes per pixel
+                int pixelSize = 3;
+
+                for (int y = 0; y < original.Height; y++)
+                {
+                    //get the data from the original image
+                    byte* oRow = (byte*)originalData.Scan0 + (y * originalData.Stride);
+
+                    //get the data from the new image
+                    byte* nRow = (byte*)newData.Scan0 + (y * newData.Stride);
+
+                    for (int x = 0; x < original.Width; x++)
+                    {
+                        //create the grayscale version
+                        byte grayScale =
+                           (byte)((oRow[x * pixelSize] * .11) + //B
+                           (oRow[x * pixelSize + 1] * .59) +  //G
+                           (oRow[x * pixelSize + 2] * .3)); //R
+
+                        //set the new image's pixel to the grayscale version
+                        nRow[x * pixelSize] = grayScale; //B
+                        nRow[x * pixelSize + 1] = grayScale; //G
+                        nRow[x * pixelSize + 2] = grayScale; //R
+                    }
+                }
+
+                //unlock the bitmaps
+                newBitmap.UnlockBits(newData);
+                original.UnlockBits(originalData);
+
+                return newBitmap;
+            }
+        }
+    }
+}
